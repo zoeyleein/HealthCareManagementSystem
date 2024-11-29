02f619e63ec15c5408b748b0951d0ea46cc58e25
Cheng0124 <159956557+Cheng0124@users.noreply.github.com>
2024-11-29 14:04:55 -0500

    1st Commit

---
+++ src/main/java/com/hcms/healthcaremanagementsystem/Model/Appointment.java
@@ -0,0 +1,51 @@
+package com.hcms.healthcaremanagementsystem.Model;
+
+public class Appointment {
+    private int id;
+    private int patientId;
+    private String doctorName;
+    private String appointmentDate;
+    private String appointmentTime;
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(int patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getDoctorName() {
+        return doctorName;
+    }
+
+    public void setDoctorName(String doctorName) {
+        this.doctorName = doctorName;
+    }
+
+    public String getAppointmentDate() {
+        return appointmentDate;
+    }
+
+    public void setAppointmentDate(String appointmentDate) {
+        this.appointmentDate = appointmentDate;
+    }
+
+    public String getAppointmentTime() {
+        return appointmentTime;
+    }
+
+    public void setAppointmentTime(String appointmentTime) {
+        this.appointmentTime = appointmentTime;
+    }
+}
+
